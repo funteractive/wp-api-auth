d81152c452cda30b49383a4aa1d29abab1217cf8
Keisuke Imura <keisuke@keisuke-imura.com>
2015-10-31 16:22:15 +0900

    define plugin

---
+++ wp-api-auth.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP API Auth
+Plugin URI: https://funteractive.co.jp/
+Description: This plugin provide pages to connect many services with apis in wp-admin.
+Author: FUNTERACTIVE, Inc.
+Author URI: https://funteractive.co.jp
+Text Domain: wp-api-auth
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+Version: 0.1
+*/
