bde2a9ce4051421d8623d4ca58e180c8ddd9c3e6
Keisuke Imura <keisuke@keisuke-imura.com>
2015-11-07 16:13:57 +0900

    get profile test

--- services/google-analytics.php
+++ services/google-analytics.php
@@ -13,8 +13,8 @@ class WpApiAuth_GA
     session_start();
     $this->client = new Google_Client();
     $this->client->setAuthConfigFile( WP_API_AUTH_DIR . 'client_secrets.json' );
-    $this->client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/wp-admin/options-general.php?page=wp-api-auth');
-    $this->client->setScopes( 'https://www.googleapis.com/auth/analytics.readonly' );
+    $this->client->setRedirectUri( 'http://' . $_SERVER['HTTP_HOST'] . '/wp-admin/options-general.php?page=wp-api-auth' );
+    $this->client->setScopes( Google_Service_Analytics::ANALYTICS_READONLY );
 
     if( isset( $_GET['code'] ) ) {
       $this->client->authenticate( $_GET['code'] );
@@ -24,20 +24,19 @@ class WpApiAuth_GA
 
   public function render_admin_page() {
     if( isset( $_SESSION['access_token'] ) ) {
-      $data = $this->getUserFromToken( $_SESSION['access_token'] );
-      var_dump($data);
+      $this->service = new Google_Service_Analytics( $this->client );
+      $startindex = 1;
+      $profiles = $this->service->management_profiles->listManagementProfiles( '~all', '~all', array( 'start-index' => $startindex ) );
+
+      $items = $profiles->getItems();
+
+      $totalresults = $profiles->getTotalResults();
+      var_dump($profiles);
+      var_dump($items);
+      var_dump($totalresults);
     } else {
       $authUrl = $this->client->createAuthUrl();
       echo '<a href="' . $authUrl . '" target="_blank">auth</a>';
     }
-  }
-
-  public function getUserFromToken( $token ) {
-    $ticket = $this->client->verifyIdToken( $token );
-    if ( $ticket ) {
-      $data = $ticket->getAttributes();
-      return $data; // user ID
-    }
-    return false;
   }
 }
