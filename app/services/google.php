4bdeaeb96f71ccbb2bbb4048b81c2155b1b9d4ec
Keisuke Imura <keisuke@keisuke-imura.com>
2015-11-08 00:29:04 +0900

    add refresh method

--- app/services/google.php
+++ app/services/google.php
@@ -9,7 +9,6 @@ if ( !defined( 'ABSPATH' ) )
  */
 class WpApiAuth_Google
 {
-
   private $service_name = 'google';
 
   public function __construct() {
@@ -31,19 +30,23 @@ class WpApiAuth_Google
       try {
         $this->client->setAccessToken( $_SESSION['access_token'] );
         $this->service = new Google_Service_Analytics( $this->client );
-
-        // Get Google Analytics accounts.
-        $accounts = $this->service->management_accounts->listManagementAccounts();
       } catch( Google_Exception $e ) {
-        unset( $_SESSION['access_token'] );
+        $this->refresh();
         echo $e->getMessage();
       }
+
+      // Get Google Analytics accounts.
+      $accounts = $this->service->management_accounts->listManagementAccounts();
+      var_dump( $accounts );
     } else {
       $authUrl = $this->client->createAuthUrl();
       echo '<a class="button button-secondary" href="' . $authUrl . '" target="_blank">Authorized Plugin</a>';
     }
   }
 
+  /**
+   * Set client data.
+   */
   private function set_client() {
     $this->client = new Google_Client();
     $this->client->setAuthConfigFile( WP_API_AUTH_DIR . 'client_secrets.json' );
@@ -53,13 +56,19 @@ class WpApiAuth_Google
     $this->client->setApprovalPrompt( 'force' );
   }
 
+  /**
+   * Authenticate and get access token.
+   * @param $code
+   */
   private function authenticate( $code ) {
     $this->client->authenticate( $code );
     try {
       $access_token = $this->client->getAccessToken();
       if( $access_token ) {
         # TODO : Show update message.
+
         $this->save_access_token( $access_token );
+        $_SESSION['access_token'] = $access_token;
       }
     } catch( Google_Exception $e ) {
       echo $this->helper( $e->getMessage() );
@@ -78,6 +87,28 @@ class WpApiAuth_Google
     } else {
       add_option( $option_name, serialize( $access_token ) );
     }
+  }
+
+  /**
+   * Get access_token from DB.
+   * @return mixed|void
+   */
+  private function get_access_token() {
+    $option_name = $this->helper->get_token_option_name( $this->service_name );
+    return unserialize( get_option( $option_name ) );
+  }
+
+  /**
+   * Refresh access token.
+   * @return bool
+   */
+  private function refresh() {
+    $token = $this->get_access_token();
+    if( !isset( $token['refresh_token'] ) || !$token['refresh_token'] ) {
+      return false;
+    }
+
+    $this->client->refreshToken( $token['refresh_token'] );
   }
 
 }
