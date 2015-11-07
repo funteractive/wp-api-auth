e93d69d75cd7e54ce78a7dcf9bfc6a61e5ea9bdb
Keisuke Imura <keisuke@keisuke-imura.com>
2015-11-07 14:26:58 +0900

    add load

--- services/google-analytics.php
+++ services/google-analytics.php
@@ -9,7 +9,7 @@ class WpApiAuth_GA
 
   public function __construct() {
     //require_once ( WP_API_AUTH_DIR . 'tools/autoload.php' );
-    //require_once ( WP_API_AUTH_DIR . 'vendor/google-api-php-client/src/Google/Client.php' );
+    require_once ( WP_API_AUTH_DIR . 'vendor/google-api-php-client/src/Google/Client.php' );
 
     session_start();
     $this->client = new Google_Client();
