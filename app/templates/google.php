ac0169654d6fb455e62319237a77b2955fc88f68
Keisuke Imura <keisuke@keisuke-imura.com>
2015-11-20 20:46:55 +0900

    delete var_dump

--- app/templates/google.php
+++ app/templates/google.php
@@ -7,8 +7,6 @@ if ( !defined( 'ABSPATH' ) )
 // Include Google Class
 require_once( WP_API_AUTH_DIR . 'app/services/google.php' );
 $google = new WpApiAuth_Google();
-
-var_dump($_POST);
 ?>
 
 <h3><?php _e( 'Google Analytics' ); ?></h3>
